5ce73fe6f29d001169402a5a59a9383e46bf2040
IsabellaMatos44801 <isabella.messias@sptech.school>
2024-05-23 15:56:25 -0300

    atualizacoes

---
+++ js/script_cadastro.js
@@ -0,0 +1,19 @@
+function cadastrar(){
+    
+const nome = input_nome.value
+const email = input_email.value
+const senha = input_senha.value
+const confirmeSenha = input_confirme_senha.value
+
+let nomeValido = nome.length
+let nomeTemNaLista = true
+listaCaracteres = ['!', '@', '$', '%', '&', '*']
+
+for(let letrar =0){
+
+}
+
+if(nomeValido < 6 || nomeValido !=listaCaracteres){
+    alert(``)
+}
+}
